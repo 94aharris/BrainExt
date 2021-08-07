0c4b32331d597a5d86e2bdbf6881a4fc5cfb517a
Anthony Harris <94aharris@gmail.com>
2021-08-07 16:30:46 -0400

    binary tree height (python)

---
+++ Python/HakerRanker/BinaryTreeHeight.py
@@ -0,0 +1,81 @@
+# https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=trees
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+
+def height(root):
+    maxDepth = 0
+    maxLeft = 0
+    maxRight = 0
+    
+    if (root) :
+        
+        if (root.left) :
+            maxLeft = 1
+            maxLeft += height(root.left)
+        
+        if (root.right) :
+            maxRight = 1
+            maxRight += height(root.right)
+        
+        maxDepth = max(maxLeft, maxRight)
+        
+        return maxDepth
+    
+    
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+print(height(tree.root))
